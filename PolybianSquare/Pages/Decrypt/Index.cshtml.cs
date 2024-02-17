1262b205411765dcd2a972b4dcc84c40afe49cc4
adef <basc11111@gmail.com>
2024-02-17 20:34:10 +0300

    add encrypt and decrypt page

---
+++ PolybianSquare/Pages/Decrypt/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PolybianSquare.Pages.Decrypt;
+
+public class Index : PageModel
+{
+    [BindProperty]
+    public string? EncryptedText { get; set; }
+    public string? DecryptedText { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        DecryptedText = EncryptedText;
+        return Page();
+    }
+}
---
+++ PolybianSquare/Pages/Encrypt/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PolybianSquare.Pages.Encrypt;
+
+public class Index : PageModel
+{
+    [BindProperty] public string? OpenText { get; set; }
+    public string? EncryptedText { get; set; }
+
+    public void OnGet()
+    {
+        
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+
+        EncryptedText = OpenText;
+        return Page();
+    }
+}
