b5545152781db3c62a7c8bb964f515d712415699
adef <basc11111@gmail.com>
2024-02-26 20:28:44 +0300

    add square page

---
+++ PolybianSquare/Pages/Square/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PolybianSquare.Services;
+
+namespace PolybianSquare.Pages.Square;
+
+public class Index : PageModel
+{
+    private ICryptService _cryptService;
+
+    public Index(ICryptService cryptService)
+    {
+        _cryptService = cryptService;
+    }
+
+    [BindProperty] public string? OpenText { get; set; }
+    public string? EncryptedText { get; set; }
+
+    [BindProperty] public string? EncryptedMyText { get; set; }
+    public string? DecryptedText { get; set; }
+
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostEncryptAsync()
+    {
+        if (OpenText == null)
+            return Page();
+
+        EncryptedText = await _cryptService.Encrypted(OpenText);
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostDecryptAsync()
+    {
+        if (EncryptedMyText == null)
+            return Page();
+
+        DecryptedText = await _cryptService.Decrypted(EncryptedMyText);
+        return Page();
+    }
+}
