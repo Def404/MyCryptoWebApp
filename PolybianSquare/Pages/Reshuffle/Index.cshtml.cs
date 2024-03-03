6643bf59cae7d235663628ca6145ad8ee219346c
Def404 <basc11111@gmail.com>
2024-03-03 11:29:52 +0300

    Add new page for method

---
+++ PolybianSquare/Pages/Reshuffle/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PolybianSquare.Pages.Reshuffle;
+
+public class Index : PageModel
+{
+	[BindProperty] public string? OpenText { get; set; }
+	public string? EncryptedText { get; set; }
+	[BindProperty] public string? EncryptedMyText { get; set; }
+	public string? DecryptedText { get; set; }
+	
+	public void OnGet()
+	{
+		
+	}
+	
+	public async Task<IActionResult> OnPostEncryptAsync()
+	{
+		if (OpenText == null)
+			return Page();
+		
+		return Page();
+	}
+
+	public async Task<IActionResult> OnPostDecryptAsync()
+	{
+		if (EncryptedMyText == null)
+			return Page();
+		
+		
+		return Page();
+	}
+}
